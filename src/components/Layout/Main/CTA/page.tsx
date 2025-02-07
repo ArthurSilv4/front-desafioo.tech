da14bcdce7caece583aae5c1fdd17bd1930b0992
Arthur de Souza Silva <arthurdeosuza.silv@gmail.com>
2025-02-07 17:03:56 -0300

    add icons

--- src/components/Layout/Main/CTA/page.tsx
+++ src/components/Layout/Main/CTA/page.tsx
@@ -28,13 +28,27 @@ export function CTA() {
             </Link>
           </div>
           <div className="hidden md:block">
-            <Image
-              src="/eu.png"
-              width={200}
-              height={200}
-              alt="Desafioo.Tech"
-              className="w-full"
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="250"
+              height="250"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-swords"
+            >
+              <polyline points="14.5 17.5 3 6 3 3 6 3 17.5 14.5" />
+              <line x1="13" x2="19" y1="19" y2="13" />
+              <line x1="16" x2="20" y1="16" y2="20" />
+              <line x1="19" x2="21" y1="21" y2="19" />
+              <polyline points="14.5 6.5 18 3 21 3 21 6 17.5 9.5" />
+              <line x1="5" x2="9" y1="14" y2="18" />
+              <line x1="7" x2="4" y1="17" y2="20" />
+              <line x1="3" x2="5" y1="19" y2="21" />
+            </svg>
           </div>
         </div>
       </div>
--- src/components/Layout/Main/StartProgramming/page.tsx
+++ src/components/Layout/Main/StartProgramming/page.tsx
@@ -34,13 +34,22 @@ export function StartProgramming() {
               ))}
           </div>
           <div className="hidden md:block">
-            <Image
-              src="/eu.png"
-              alt="Programação"
-              width={200}
-              height={200}
-              className="w-full md:w-96"
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="250"
+              height="250"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-code-xml"
+            >
+              <path d="m18 16 4-4-4-4" />
+              <path d="m6 8-4 4 4 4" />
+              <path d="m14.5 4-5 16" />
+            </svg>
           </div>
         </div>
       </div>

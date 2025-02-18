4848f8121bbdddce297440bd94cfcd12f1b83c6c
Arthur de Souza Silva <arthurdesouza.silv@gmail.com>
2025-02-18 18:02:39 -0300

    criado componente de carregamento do desafio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,8 @@
-"use client";
-
 import Main from "@/components/Layout/Main/page";
-import { Card, CardContent } from "@/components/ui/card";
-import { useChallenge } from "@/context/challenges/page";
+import { CardChallengeList } from "@/components/Lists/CardChallengeList";
 import { Separator } from "@radix-ui/react-separator";
-import { Camera, Code2Icon } from "lucide-react";
-import Link from "next/link";
 
 export default function Home() {
-  const { useFetchChallenge } = useChallenge();
-
-  const { data: challengeList, isLoading } = useFetchChallenge();
-
-  if (isLoading || !challengeList) {
-    return (
-      <Main>
-        <section className="min-h-screen" id="challenge">
-          <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold">Desafios</h1>
-            <Separator className="bg-gray-300 h-[1px] my-4" />
-
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
-              <Link href={""}>
-                <Card className="hover:shadow-lg transition-shadow mb-4 w-72 2xl:w-80">
-                  <CardContent className="p-4">
-                    <div className="flex items-start gap-3 w-full">
-                      <div className="rounded-lg p-2 bg-teal-100">
-                        <Code2Icon />
-                      </div>
-                      <div className="space-y-1 w-full truncate">
-                        <h2 className="text-base font-medium text-blue-950 truncate">
-                          Carregando...
-                        </h2>
-                        <div className="flex items-center gap-1 text-gray-500 text-sm">
-                          <Camera className="w-4 h-4" />
-                          <span className="inline-block">Carregando...</span>
-                        </div>
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </Link>
-            </div>
-          </div>
-        </section>
-      </Main>
-    );
-  }
   return (
     <Main>
       <section className="min-h-screen" id="challenge">
@@ -55,34 +11,7 @@ export default function Home() {
           <Separator className="bg-gray-300 h-[1px] my-4" />
 
           <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
-            {challengeList.map((challenge, index) => (
-              <Link href={`/${challenge.id}`} key={index}>
-                <Card className="hover:shadow-lg transition-shadow mb-4 w-72 2xl:w-80">
-                  <CardContent className="p-4">
-                    <div className="flex items-start gap-3 w-full">
-                      <div className="rounded-lg p-2 bg-teal-100">
-                        <Code2Icon />
-                      </div>
-                      <div className="space-y-1 w-full truncate">
-                        <h2 className="text-base font-medium text-blue-950 truncate">
-                          {challenge.title}
-                        </h2>
-                        <div className="flex items-center gap-1 text-gray-500 text-sm">
-                          <Camera className="w-4 h-4" />
-                          <span className="inline-block">
-                            {`Mais de ${
-                              challenge.starts === 0
-                                ? Math.floor(Math.random() * 3) + 1
-                                : challenge.starts
-                            } pessoas já começaram`}
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </Link>
-            ))}
+            <CardChallengeList />
           </div>
         </div>
       </section>
---
+++ src/components/Lists/CardChallengeList.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { useChallenge } from "@/context/challenges/page";
+import { Camera, Code2Icon } from "lucide-react";
+import Link from "next/link";
+
+export function CardChallengeList() {
+  const { useFetchChallenge } = useChallenge();
+  const { data: challengeList, isLoading } = useFetchChallenge();
+
+  if (isLoading || !challengeList) {
+    return (
+      <>
+        {[...Array(8)].map((_, index) => (
+          <div key={index}>
+            <Card className="w-72 2xl:w-80">
+              <CardContent className="p-4">
+                <div className="animate-pulse flex items-start gap-3 w-full">
+                  <div className="rounded-lg p-2 bg-teal-100">
+                    <Code2Icon />
+                  </div>
+                  <div className="space-y-1 w-full truncate">
+                    <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+                    <div className="h-3 bg-gray-200 rounded w-1/2"></div>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+      </>
+    );
+  }
+  return (
+    <>
+      {challengeList.map((challenge, index) => (
+        <Link href={`/${challenge.id}`} key={index}>
+          <Card className="hover:shadow-lg transition-shadow mb-4 w-72 2xl:w-80">
+            <CardContent className="p-4">
+              <div className="flex items-start gap-3 w-full">
+                <div className="rounded-lg p-2 bg-teal-100">
+                  <Code2Icon />
+                </div>
+                <div className="space-y-1 w-full truncate">
+                  <h2 className="text-base font-medium text-blue-950 truncate">
+                    {challenge.title}
+                  </h2>
+                  <div className="flex items-center gap-1 text-gray-500 text-sm">
+                    <Camera className="w-4 h-4" />
+                    <span className="inline-block">
+                      {`Mais de ${
+                        challenge.starts === 0
+                          ? Math.floor(Math.random() * 3) + 1
+                          : challenge.starts
+                      } pessoas já começaram`}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Link>
+      ))}
+    </>
+  );
+}

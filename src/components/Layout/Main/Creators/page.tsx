014028e6d9daa15621c2ec582e5e28ed5beb8a9a
Arthur de Souza Silva <arthurdesouza.silv@gmail.com>
2025-03-17 15:19:17 -0300

    editado section de creators no loading

--- src/components/Layout/Main/Creators/page.tsx
+++ src/components/Layout/Main/Creators/page.tsx
@@ -11,7 +11,27 @@ export function Creators() {
   console.log(creators);
 
   if (isLoading || !creators) {
-    return <p>Carregando...</p>;
+    return (
+      <section className="py-12 mb-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-5xl font-bold text-center mb-8">
+            Mentes por trás dos desafios
+          </h2>
+          <Separator className="h-[1px] bg-gray-300 mb-8" />
+
+          <div className="flex flex-wrap justify-center gap-2">
+            <Link href={``}>
+              <div className="min-w-56 flex flex-col items-center">
+                <h3 className="text-2xl font-bold text-center text-blue-900">
+                  Carregando...
+                </h3>
+              </div>
+            </Link>
+          </div>
+          <Separator className="h-[1px] bg-gray-300 mt-8" />
+        </div>
+      </section>
+    );
   }
 
   return (

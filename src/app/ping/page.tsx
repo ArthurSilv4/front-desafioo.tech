a0e9dc1d06cc06d63999fa52d8ab6312239939f8
Arthur de Souza Silva <arthurdesouza.silv@gmail.com>
2025-04-13 13:32:51 -0300

    refatorar o componente Ping para lidar com estados de carregamento e erro e melhorar o gerenciamento do localStorage

--- src/app/ping/page.tsx
+++ src/app/ping/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
@@ -20,7 +21,6 @@ export default function Ping() {
         refetchOnWindowFocus: true,
         refetchInterval: 300000,
         staleTime: 300000,
-
         onError: (error) => {
           console.error(error);
         },
@@ -28,18 +28,26 @@ export default function Ping() {
     );
   };
 
-  const { data: pingData } = useFetchPing();
+  const { data: pingData, isLoading, error } = useFetchPing();
 
-  if (pingData) {
-    localStorage.setItem("lastPing", pingData);
-  }
+  useEffect(() => {
+    if (pingData) {
+      localStorage.setItem("lastPing", pingData);
+    }
+  }, [pingData]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading ping.</p>;
+
+  const lastPing =
+    typeof window !== "undefined"
+      ? localStorage.getItem("lastPing") || "No ping saved yet."
+      : "No ping saved yet.";
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
       <h1>Ping</h1>
-      <p>
-        Ultimo ping: {localStorage.getItem("lastPing") || "No ping saved yet."}
-      </p>
+      <p>Ultimo ping: {lastPing}</p>
     </main>
   );
 }
